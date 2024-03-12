3216817322cb367030f4f26f9f6c97c05a342adb
ksdyb.park <ksdyb@amazon.com>
2024-03-12 19:44:30 +0900

    update

---
+++ cdk-demo-multimodal/test/cdk-demo-multimodal.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDemoMultimodal from '../lib/cdk-demo-multimodal-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-demo-multimodal-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDemoMultimodal.CdkDemoMultimodalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
