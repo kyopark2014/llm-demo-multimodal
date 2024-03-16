de3c8bc267b557b23046940dd7ba322dd1c9d721
ksdyb.park <ksdyb@amazon.com>
2024-03-16 19:20:14 +0900

    update

--- lambda-redis/lambda_function.py
+++ lambda-redis/lambda_function.py
@@ -14,25 +14,34 @@ redisAddress = os.environ.get('redisAddress')
 redisPort = os.environ.get('redisPort')
 
 try: 
-    rd = redis.StrictRedis(host=redisAddress, port=redisPort, db=0)    
-    
+    rd = redis.StrictRedis(host=redisAddress, port=redisPort, db=0)        
 except Exception:
     err_msg = traceback.format_exc()
     print('error message: ', err_msg)                    
     raise Exception ("Not able to request to LLM")
     
 def lambda_handler(event, context):
-    print(event)
+    userId = event['userId']     
+    query = event['query']
+    print(f'userId: {userId}, query: {query}')
     
+    channel = f"{userId}"    
+    try: 
+        rd.publish(channel=channel, message=json.dumps(query))
+        print('successfully published: ', json.dumps(query))
     
-    
-    
+    except Exception:
+        err_msg = traceback.format_exc()
+        print('error message: ', err_msg)                    
+        raise Exception ("Not able to request to LLM")
+        
     msg = "success"
     
     return {
         "isBase64Encoded": False,
         'statusCode': 200,
-        'body': json.dumps({            
-            "msg": msg
+        'body': json.dumps({ 
+            "channel": channel,
+            "query": json.dumps(query)
         })
     }
