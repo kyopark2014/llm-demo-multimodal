6fc2cc926819ba17205828509f63e52c40ce53ef
ksdyb.park <ksdyb@amazon.com>
2024-03-18 08:00:23 +0900

    update

--- lambda-voice-ws/lambda_function.py
+++ lambda-voice-ws/lambda_function.py
@@ -143,7 +143,7 @@ def lambda_handler(event, context):
         if routeKey == '$connect':
             print('connected!')
             
-            deliveryVoiceMessage("general", "hello world!")
+            #deliveryVoiceMessage("general", "hello world!")
             
             print('start subscribing redis.')
             channel = 'kyopark'    
