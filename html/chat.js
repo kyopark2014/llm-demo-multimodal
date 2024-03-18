0c3821df26b9f98eff8661e11869a3081013ed7a
ksdyb.park <ksdyb@amazon.com>
2024-03-19 08:51:21 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -288,6 +288,7 @@ function voiceConnect(voiceEndpoint, type) {
                 let requestTime = datastr+' '+timestr
 
                 let previous = listMessages.get(requestId); 
+                console.log('previosu: ', previous)
                 //console.log('length: (previous)'+previous.length+', new:'+query.length);
                 if(query.length > previous.length) {
                     addSentMessage(requestId, timestr, query);
