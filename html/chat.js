a4eccd56a976d9b4c39d03508944a8050d269cbf
ksdyb.park <ksdyb@amazon.com>
2024-03-20 02:06:04 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -665,12 +665,11 @@ function generateVoiceWord(text) {
     xhr.send(blob);            
 }
 
-
 function playAudio(text) {
     const uri = "speech";
     const xhr = new XMLHttpRequest();
 
-    let speechId = userId;
+    let fname = userId+'.mp3';
     let voiceId = 'Seoyeon';
     // voiceId: 'Aditi'|'Amy'|'Astrid'|'Bianca'|'Brian'|'Camila'|'Carla'|'Carmen'|'Celine'|'Chantal'|'Conchita'|'Cristiano'|'Dora'|'Emma'|'Enrique'|'Ewa'|'Filiz'|'Gabrielle'|'Geraint'|'Giorgio'|'Gwyneth'|'Hans'|'Ines'|'Ivy'|'Jacek'|'Jan'|'Joanna'|'Joey'|'Justin'|'Karl'|'Kendra'|'Kevin'|'Kimberly'|'Lea'|'Liv'|'Lotte'|'Lucia'|'Lupe'|'Mads'|'Maja'|'Marlene'|'Mathieu'|'Matthew'|'Maxim'|'Mia'|'Miguel'|'Mizuki'|'Naja'|'Nicole'|'Olivia'|'Penelope'|'Raveena'|'Ricardo'|'Ruben'|'Russell'|'Salli'|'Seoyeon'|'Takumi'|'Tatyana'|'Vicki'|'Vitoria'|'Zeina'|'Zhiyu'|'Aria'|'Ayanda'|'Arlet'|'Hannah'|'Arthur'|'Daniel'|'Liam'|'Pedro'|'Kajal'|'Hiujin'|'Laura'|'Elin'|'Ida'|'Suvi'|'Ola'|'Hala'|'Andres'|'Sergio'|'Remi'|'Adriano'|'Thiago'|'Ruth'|'Stephen'|'Kazuha'|'Tomoko'
 
@@ -694,7 +693,7 @@ function playAudio(text) {
         "text": text,
         "voiceId": voiceId,
         "langCode": langCode,
-        "speechId": speechId
+        "fname": fname
     }
     console.log("request: " + JSON.stringify(requestObj));
 
